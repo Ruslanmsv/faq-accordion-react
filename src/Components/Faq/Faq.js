44af0deaa892b3b2db5b0213337a325136303280
Ruslan Musaev <ruslan.msv@gmail.com>
2022-09-19 23:44:52 +0200

    Wrapping FAQ in <main> landmark tag for accessability

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -4,7 +4,7 @@ import s from './Faq.module.css';
 
 const Faq = props => {
     return (
-        <div className={s.container}>
+        <main className={s.container}>
             <div className={s.picture}>
                 <div className={s.box}></div>
             </div>
@@ -12,7 +12,7 @@ const Faq = props => {
                 <h1 className={s.title}>FAQ</h1>
                 <Accordion data={props.data} />
             </div>
-        </div>
+        </main>
     )
 };
 
